9fac43d0924476e1efd439624ad6dd9f338c2204
Lou <lou.dutertrethouan@viacesi.fr>
2025-03-01 21:17:24 +0100

    Ajout du jeu sur github
    Test de l'implémentation du jeu sur github

---
+++ Assets/Script/Class_Monster.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Class_Monster : MonoBehaviour
+{
+
+    public int Pv;
+    public float vitesse_de_deplacement;
+    public int Armure;
+    public string type_Monster;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Script/SetTextVague.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using JetBrains.Annotations;
+using UnityEditor.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine;
+using TMPro;
+public class SetTextVague: MonoBehaviour
+
+{
+    public TextMeshProUGUI vagueText; // Texte UI à assigner dans l'inspecteur
+    private ClassPerso personnage;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        GameObject cible = GameObject.Find("Personnage");
+
+        personnage = cible.GetComponent<ClassPerso>();
+
+        vagueText.text = ": " + personnage.vague.ToString();
+
+        Debug.Log("Le texte affiché est : " + vagueText.text); // Test à enlever après
+
+    }
+    private void Update()
+    {
+        vagueText.text = ": " + personnage.vague.ToString();
+    }
+}
